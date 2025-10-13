460cfadec4656bafb405f573d126e2db2462ed13
hcyk268 <vvhipt206.contact@gmail.com>
2025-10-13 18:14:05 +0700

    Page create new user

--- src/main/java/com/haipro/shop/controller/UserController.java
+++ src/main/java/com/haipro/shop/controller/UserController.java
@@ -2,11 +2,12 @@ package com.haipro.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.haipro.shop.service.UserService;
+import com.haipro.shop.domain.User;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
@@ -23,19 +24,17 @@ public class UserController {
         model.addAttribute("haivar", msg);
         return "test";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("NewUser") User dataUser) {
+        System.out.println("create user submit" + dataUser);
+        return "test";
+    }
+
 }
-
-// @RestController
-// public class UserController {
-
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
